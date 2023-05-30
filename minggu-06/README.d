7ee058c9a09915a4fb2303d56a97558b83142f79
mayaalf <106806229+mayaalf@users.noreply.github.com>
2023-05-30 18:27:50 +0700

    Create README.d

---
+++ minggu-06/README.d
@@ -0,0 +1,22 @@
+# LATIHAN DAN TUGAS PRAKTIKUM MINGGU KE-6
+
+```
+Latihan
+```
+1. [Install dan coba jalankan GO languange dengan koneksi MySQL & MongoDB](latihan-06.md)
+2. [RestfulAPI Mysql & MongoDB menggunakan GIN](latihan-RestfulAPI.md)
+```
+Tugas
+```
+1. Kerjakan [Buat 2 program menggunakan salah satu pustaka yang ada di implementasi GraphQL di Go untuk membaca data dari MySQL serta MongoDB dan memberikan respon GraphQL, 1 program untuk MySQL, 1 program untuk MongoDB.](tugas-06.md)
+```
+minggu-06
+├── 6
+├── cloudMongo
+├── pcloud6
+├── restful-api
+├── latihan-06.md
+├── latihan-RestfulAPI.md
+├── tugas-06.md
+└── README.md
+```
